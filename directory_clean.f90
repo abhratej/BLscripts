bc4f47b68d43ccd03fcd3de60674d489e818ce27
Abhratej Sahoo <abhratej.sahoo@gmail.com>
2021-08-09 21:36:25 +0200

    Updated directory clean routine It now saves results in data save directory

--- directory_clean.f90
+++ directory_clean.f90
@@ -1,11 +1,18 @@
 program directoryclean
     implicit none
 
+    CHARACTER(*), PARAMETER :: casefolder="/home/abhratej/workdir/Inc_Turb_NACA0012/SA/Re6e6/AoA10"
+
+    CALL EXECUTE_COMMAND_LINE(("mkdir "//casefolder//"/blresults/"))
+    CALL EXECUTE_COMMAND_LINE(("cp -r ./output/rfoildata/* "//casefolder//"/blresults/"))
+    CALL EXECUTE_COMMAND_LINE(("cp -r ./output/*.txt "//casefolder//"/blresults/"))
     CALL EXECUTE_COMMAND_LINE("rm -rf ./*.vtk")
     CALL EXECUTE_COMMAND_LINE("rm -rf ./output/*.txt")
     CALL EXECUTE_COMMAND_LINE("rm -rf ./output/*.tsv")
     CALL EXECUTE_COMMAND_LINE("rm -rf ./output/normalflowfields")
     CALL EXECUTE_COMMAND_LINE("rm -rf ./output/rfoildata/*")
     CALL EXECUTE_COMMAND_LINE("rm -rf ./output/xfoildata/*")
+    CALL EXECUTE_COMMAND_LINE("rm -rf ./fortran_compile_files/*")
+
 
 end program directoryclean
